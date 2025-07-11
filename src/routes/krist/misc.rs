bd56f2e704bfffafcfb71ebb7a6c43eeb39da1d9
bananasov <56086819+bananasov@users.noreply.github.com>
2025-07-11 21:22:00 +0200

    feat: Implement 99% of misc krist routes.
    We are just missing the supply route, lol.

---
+++ src/routes/krist/misc.rs
@@ -0,0 +1,108 @@
+use actix_web::{HttpResponse, get, post, web};
+
+use crate::{
+    AppState,
+    database::wallet::Model as Wallet,
+    errors::krist::KristError,
+    models::{
+        auth::{AddressAuthenticationResponse, LoginDetails},
+        misc::{PrivateKeyAddressResponse, WalletVersionResponse},
+        motd::{Constants, CurrencyInfo, DetailedMotd, DetailedMotdResponse, PackageInfo},
+    },
+    utils::crypto,
+};
+
+#[post("/login")]
+async fn login_address(
+    state: web::Data<AppState>,
+    query: web::Json<LoginDetails>,
+) -> Result<HttpResponse, KristError> {
+    let db = &state.pool;
+    let query = query.into_inner();
+
+    let private_key = query.private_key;
+    let result = Wallet::verify_address(db, private_key).await?;
+
+    Ok(HttpResponse::Ok().json(AddressAuthenticationResponse {
+        address: result.authed.then_some(result.address.address),
+        authed: result.authed,
+        ok: true,
+    }))
+}
+
+#[get("/motd")]
+async fn get_motd() -> HttpResponse {
+    // This is by far the simplest fucking route in all of Kromer.
+    // TODO: Make this actually better.
+    let motd = DetailedMotd {
+        server_time: "server_time".to_string(),
+        motd: "Message of the day".to_string(),
+        set: None,
+        motd_set: None,
+        public_url: "http://kromer.reconnected.cc".to_string(),
+        public_ws_url: "http://kromer.reconnected.cc/api/krist/ws".to_string(),
+        mining_enabled: false,
+        transactions_enabled: true,
+        debug_mode: true,
+        work: 500,
+        last_block: None,
+        package: PackageInfo {
+            name: "Kromer".to_string(),
+            version: "0.2.0".to_string(),
+            author: "ReconnectedCC Team".to_string(),
+            license: "GPL-3.0".to_string(),
+            repository: "https://github.com/ReconnectedCC/kromer/".to_string(),
+        },
+        constants: Constants {
+            wallet_version: 3,
+            nonce_max_size: 500,
+            name_cost: 500,
+            min_work: 50,
+            max_work: 500,
+            work_factor: 500.0,
+            seconds_per_block: 5000,
+        },
+        currency: CurrencyInfo {
+            address_prefix: "k".to_string(),
+            name_suffix: "kro".to_string(),
+            currency_name: "Kromer".to_string(),
+            currency_symbol: "KRO".to_string(),
+        },
+        notice: "Some awesome notice will go here".to_string(),
+    };
+
+    let motd = DetailedMotdResponse { ok: true, motd };
+
+    HttpResponse::Ok().json(motd)
+}
+
+#[get("/walletversion")]
+async fn get_walletversion() -> HttpResponse {
+    let response = WalletVersionResponse {
+        ok: true,
+        wallet_version: 3,
+    };
+
+    HttpResponse::Ok().json(response)
+}
+
+#[post("/v2")]
+async fn get_v2_address(query: web::Json<LoginDetails>) -> Result<HttpResponse, KristError> {
+    let query = query.into_inner();
+    let key = query.private_key;
+
+    let address = crypto::make_v2_address(&key, "k");
+    let response = PrivateKeyAddressResponse { address, ok: true };
+
+    Ok(HttpResponse::Ok().json(response))
+}
+
+pub fn config(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::scope("")
+            .service(login_address)
+            .service(get_motd)
+            // .service(get_kromer_supply)
+            .service(get_v2_address),
+    );
+}
--- src/routes/krist/mod.rs
+++ src/routes/krist/mod.rs
@@ -1,3 +1,4 @@
+mod misc;
 mod wallet;
 
 use actix_web::web;
@@ -6,8 +7,8 @@ pub fn config(cfg: &mut web::ServiceConfig) {
     // cfg.service(web::scope("/lookup").configure(lookup::config));
 
     cfg.configure(wallet::config);
+    cfg.configure(misc::config);
     // cfg.configure(transactions::config);
     // cfg.configure(ws::config);
     // cfg.configure(names::config);
-    // cfg.configure(misc::config);
 }
