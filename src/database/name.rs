0054eab67b823eedaafafe27ba104045381f1789
bananasov <56086819+bananasov@users.noreply.github.com>
2025-07-11 22:04:14 +0200

    feat: Implement ModelExt for name model

--- src/database/name.rs
+++ src/database/name.rs
@@ -1,7 +1,11 @@
+use async_trait::async_trait;
 use chrono::{DateTime, Utc};
 use rust_decimal::Decimal;
+use sqlx::{Pool, Postgres};
 
-#[derive(Debug, Clone, PartialEq)]
+use crate::database::ModelExt;
+
+#[derive(Debug, Clone, PartialEq, sqlx::FromRow)]
 pub struct Model {
     pub id: i32,
     pub last_transfered: Option<DateTime<Utc>>,
@@ -12,4 +16,37 @@ pub struct Model {
     pub time_registered: DateTime<Utc>,
     pub unpaid: Decimal,
     pub metadata: Option<String>,
+}
+
+#[async_trait]
+impl ModelExt for Model {
+    async fn fetch_by_id(pool: &Pool<Postgres>, id: i64) -> sqlx::Result<Option<Self>>
+    where
+        Self: Sized,
+    {
+        let q = "SELECT * FROM names WHERE id = $1";
+
+        sqlx::query_as(q).bind(id).fetch_optional(pool).await
+    }
+
+    async fn fetch_all(pool: &Pool<Postgres>, limit: i64, offset: i64) -> sqlx::Result<Vec<Self>>
+    where
+        Self: Sized,
+    {
+        let limit = limit.clamp(0, 1000);
+        let q = "SELECT * from names LIMIT $1 OFFSET $2";
+
+        sqlx::query_as(q)
+            .bind(limit)
+            .bind(offset)
+            .fetch_all(pool)
+            .await
+    }
+
+    async fn total_count(pool: &Pool<Postgres>) -> sqlx::Result<usize> {
+        let q = "SELECT COUNT(*) FROM names";
+        let result: i64 = sqlx::query_scalar(q).fetch_one(pool).await?;
+
+        Ok(result as usize)
+    }
 }
