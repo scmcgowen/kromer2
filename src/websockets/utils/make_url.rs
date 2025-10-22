00890cbeeca29af783094eeb26941b9d4e1d3118
Herr Katze <scmcgowen@gmail.com>
2025-10-22 13:02:18 -0700

    Make PUBLIC_URL be overridden by --url
    Also makes PUBLIC_URL default to SERVER_URL if not present

--- src/websockets/utils/make_url.rs
+++ src/websockets/utils/make_url.rs
@@ -1,8 +1,8 @@
 use std::env;
-
+use crate::get_args;
 use uuid::Uuid;
 
-use crate::errors::{KromerError, websocket::WebSocketError};
+use crate::errors::KromerError;
 
 pub fn make_url(uuid: Uuid) -> Result<String, KromerError> {
     let force_insecure = env::var("FORCE_WS_INSECURE").unwrap_or("true".to_owned());
@@ -11,9 +11,8 @@ pub fn make_url(uuid: Uuid) -> Result<String, KromerError> {
     } else {
         "wss"
     };
-
-    let server_url = env::var("PUBLIC_URL")
-        .map_err(|_| KromerError::WebSocket(WebSocketError::ServerConfigError))?;
+    let args = get_args();
+    let server_url = args.url.clone().unwrap_or_else(|| env::var("PUBLIC_URL").unwrap_or_else(|_| env::var("SERVER_URL").unwrap_or("localhost:8080".to_owned())));
 
     Ok(format!(
         "{schema}://{server_url}/api/krist/ws/gateway/{uuid}"
